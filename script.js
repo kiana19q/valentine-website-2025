4dbfb5267f371c3aed891dbabe57a221729a9794
Kiana Qanbari <qanbarik9@gmail.com>
2026-02-13 21:12:12 +0330

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function validateConfig() {
     // Check required fields
     if (!config.valentineName) {
         warnings.push("Valentine's name is not set! Using default.");
-        config.valentineName = "ی عزیزم";
+        config.valentineName = "🐻❤️";
     }
 
     // Validate colors
