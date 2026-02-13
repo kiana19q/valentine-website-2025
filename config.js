7b0caa1cbfe5fa4647995bdcfbf50c5d52f1f051
Kiana Qanbari <qanbarik9@gmail.com>
2026-02-13 20:28:04 +0330

    Update config.js

--- config.js
+++ config.js
@@ -81,7 +81,7 @@ const CONFIG = {
         musicUrl: "https://res.cloudinary.com/dxjilegte/video/upload/v1771001010/4_5841216614174823234_yie8ab.m4a", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
-        volume: 0.5                        // Volume level (0.0 to 1.0)
+        volume: 1.0                       // Volume level (0.0 to 1.0)
     }
 };
 
